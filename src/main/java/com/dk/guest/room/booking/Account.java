7e68a8b253ee7667cfcca8d18e35911d600178ad
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2021-11-12 00:14:06 +0530

    Create and read account (owner and customer)
    Create and read account (owner and customer)

---
+++ src/main/java/com/dk/guest/room/booking/Account.java
@@ -0,0 +1,59 @@
+package com.dk.guest.room.booking;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer accountId;
+	String type;
+	String email;
+	String mobileNumber;
+	String fullName;
+	
+	public Integer getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [accountId=" + accountId + ", type=" + type + ", email=" + email + ", mobileNumber="
+				+ mobileNumber + ", fullName=" + fullName + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/test/java/com/dk/guest/room/booking/DkGuestRoomBookingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dk.guest.room.booking;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DkGuestRoomBookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
